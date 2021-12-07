48cd4e43cb29b68fde6b83400770d945778f76a8
Ali Subhani <ali.subhani@tintash.com>
2021-12-07 08:24:41 +0500

    added code for the react redux project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./containers/header";
+import ProductListing from "./containers/productListing";
+import ProductDetail from "./containers/productDetails";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 NOT FOUND</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
