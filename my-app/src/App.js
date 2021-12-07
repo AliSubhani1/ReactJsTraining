c5461528732d2f62b853e113642393ce9b9fc54a
Ali Subhani <ali.subhani@tintash.com>
2021-12-07 21:41:54 +0500

    tried out the use of class instead of using hooks with functions in react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,31 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React from "react";
-const ToDoList = () => {
-  return (
-    <div>
-      <div className="main_div">
-        <div className="center_div">
-          <h1>0</h1>
-          <div className="btn_div">
-            <button>Increment</button>
-            <button>Decrement</button>
-          </div>
-        </div>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "Hello there",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button
+            onClick={() =>
+              this.setState({ string: "Hey, the text has been updated" })
+            }
+          >
+            Change Text
+          </button>
+        </header>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from './App';
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ToDoList from "./TodoList";
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
     <ToDoList />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
