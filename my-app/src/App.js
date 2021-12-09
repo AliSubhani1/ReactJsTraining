de75fe2df0995e567c2e8190f663eafa5a56a0cf
Ali Subhani <ali.subhani@tintash.com>
2021-12-09 09:47:31 +0500

    updated file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
     this.state = {
       Monsters: [],
       searchField: "",
+      title=""
     };
     // this.handleChange = this.handleChange.bind(this);
   }
