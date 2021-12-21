ea1035aa3b26fa026cdae36f159ef1c1560fc179
Ali Subhani <ali.subhani@tintash.com>
2021-12-21 20:31:15 +0500

    tried out different solutions for routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,40 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+const Home = () => (
+  <h1>
+    <Link to="/about">Click Me</Link>
+  </h1>
+);
+const About = () => <h1>About Us</h1>;
+
 function App() {
+  // return (
+  //   <Routes>
+  //     <Route path="/" exact component={HomePage} />
+  //     <Route path="/about" component={About} />
+  //   </Routes>
+  // );
   return (
     <div>
-      <HomePage></HomePage>
+      <Routes>
+        <Route path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Routes>
     </div>
   );
+  // return (
+  //   <div>
+  //     <HomePage></HomePage>
+  //   </div>
+  // );
 }
 
 export default App;
