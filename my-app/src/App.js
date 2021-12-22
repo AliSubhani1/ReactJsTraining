29e08cf73eb9586bdaaf04c56d742f72722e642c
Ali Subhani <ali.subhani@tintash.com>
2021-12-22 15:49:01 +0500

    added a collection preview component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,39 +2,20 @@ import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
+import ShopPage from "./pages/shop/shop.component";
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
   </div>
 );
-const Home = () => (
-  <h1>
-    <Link to="/about">Click Me</Link>
-  </h1>
-);
-const About = () => <h1>About Us</h1>;
 
 function App() {
-  // return (
-  //   <Routes>
-  //     <Route path="/" exact component={HomePage} />
-  //     <Route path="/about" component={About} />
-  //   </Routes>
-  // );
   return (
     <div>
-      <Routes>
-        <Route path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
-      </Routes>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
-  // return (
-  //   <div>
-  //     <HomePage></HomePage>
-  //   </div>
-  // );
 }
 
 export default App;
--- my-app/src/pages/homepage/homepage.component.jsx
+++ my-app/src/pages/homepage/homepage.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./homepage.styles.scss";
 import "../../components/directory/directory.component";
 import Directory from "../../components/directory/directory.component";
-const HomePage = () => (
+const HomePage = ({ history }) => (
   <div className="homepage">
-    <Directory />
+    <Directory history={history} />
   </div>
 );
 
