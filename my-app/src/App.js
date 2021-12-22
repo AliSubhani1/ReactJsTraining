9ba86abc276a4d4b5360fc75b07d0de255382883
Ali Subhani <ali.subhani@tintash.com>
2021-12-22 22:48:20 +0500

    added a component for displaying menu buttns

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
@@ -12,8 +13,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
