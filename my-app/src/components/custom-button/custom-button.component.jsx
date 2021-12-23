5668b4811d0b5a3ea4309d95aac9c05bc69a74b4
Ali Subhani <ali.subhani@tintash.com>
2021-12-23 08:19:03 +0500

    created a separate component for login button

---
+++ my-app/src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./custom-button.styles.scss";
+
+const customButton = ({ children, ...otherProps }) => (
+  <button className="custom-button" {...otherProps}>
+    {children}
+  </button>
+);
+export default customButton;
